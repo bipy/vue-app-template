85c2c7e169cc4bb68084a6d83faa3aa928337956
bipy <notbipy@gmail.com>
2022-07-05 23:29:29 +0800

    refactor: all
    BREAKING CHANGE:

---
+++ src/router/index.ts
@@ -0,0 +1,28 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import Home from '@/views/home.vue'
+import Pinia from '@/views/pinia.vue'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/pinia',
+    name: 'Pinia',
+    component: Pinia
+  },
+  {
+    path: '/axios',
+    name: 'Axios',
+    component: () => import('@/views/axios.vue') // lazy-load
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
